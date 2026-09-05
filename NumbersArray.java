8f811270e268ba310d1bf28583061c9ac1ce6930
Jade Aw <jadeaw@gmail.com>
2026-09-05 18:06:30 +0800

    1

--- NumbersArray.java
+++ NumbersArray.java
@@ -19,7 +19,60 @@ public class NumbersArray {
         System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
     }
 
-    // Write your methods here
+    public static int findMax(Integer[] arr){
+        int largest = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    public static Integer[] findDuplicates(Integer[] arr){
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i] == arr[j] && j > i){
+                    temp.add(arr[i]);
+                }
+            }
+        }
+
+        Integer[] result = new Integer[temp.size()];
+
+        for(int k = 0; k < temp.size(); k++){
+            result[k] = temp.get(k);
+        }
+
+        return result;
+    }
+
+    public static Integer[] findUnique(Integer[] arr){
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            boolean duplicate = false;
+            for(int j = 0; j < arr.length; j++){
+                if(arr[i] == arr[j] && j != i){
+                    duplicate = true;
+                    break;
+                }
+            }
+            if(!duplicate){
+                temp.add(arr[i]);
+            }
+            
+        }
+
+        Integer[] result = new Integer[temp.size()];
+
+        for(int k = 0; k < temp.size(); k++){
+            result[k] = temp.get(k);
+        }
+
+        return result;
+
+    }
     
 }
 
